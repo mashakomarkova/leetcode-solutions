bc4f329aae6ec270a51e5ffef604e1354652f29a
mashakomarkova <masha.komarkofffa@gmail.com>
2022-12-05 21:26:30 +0100

    Palindrome task is added

---
+++ leetcode/src/main/java/com/leetcode/Palindrome.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+public class Palindrome {
+
+    private static final int DIVIDER = 10;
+
+    public static void main(String[] args) {
+        System.out.println(new Palindrome().isPalindrome(101));
+    }
+
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % DIVIDER == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            int t = x % DIVIDER;
+            x /= DIVIDER;
+            revertedNumber = revertedNumber * DIVIDER + t;
+
+        }
+        return x == revertedNumber || x == revertedNumber / DIVIDER;
+    }
+}
